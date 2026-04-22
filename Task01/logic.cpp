3411b26dd8758f9e2e5a28ba3bf7c8a931d95282
Gimbol4 <Shishkanov.yd@gmail.com>
2026-04-22 17:08:51 +0300

    Add project files.

---
+++ Task01/logic.cpp
@@ -0,0 +1,30 @@
+﻿// Task 01 [The arithmetic mean of non-zero elements]
+// Среднее арифметическое ненулевых элементов
+// 
+// Дана математическая прямоугольная матрица размером N на M.
+// Необходимо разработать функцию (или программу), которая 
+// вычисляет среднее арифметическое ненулевых элементов матрицы.
+
+#include "logic.h"
+
+double calculate_arithmetical_mean_of_nonzero_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
+	if (n < 1 || m < 1) {
+		return -1;
+	}
+	double mid = 0.0;
+	int k = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (matrix[i][j] != 0) {
+				mid += matrix[i][j];
+				k++;
+			}
+		}
+	}
+	if (k == 0) {
+		return 0;
+	}
+	return mid / k;
+}
---
+++ Task02/logic.cpp
@@ -0,0 +1,26 @@
+﻿// Task 02 [The sum of elements]
+// Сумма элементов 
+// 
+// Дана математическая квадратная матрица размером N.
+// Необходимо разработать функцию (или программу), 
+// которая вычисляет сумму элементов матрицы, 
+// расположенных на главной и побочной диагоналях.
+#include "logic.h"
+
+int sum_main_and_second_diagonales_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+	if (n < 1) {
+		return -1;
+	}
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += matrix[i][i];
+	}
+
+	for (int i = n; i > 0; i++)
+	{
+		sum += matrix[i][i];
+	}
+
+	return sum;
+}
